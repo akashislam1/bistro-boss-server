a51b15a5b399bd599464e8c52b055bf7928f9aca
akashislam1 <m.naimul110@gmail.com>
2023-05-28 20:24:31 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 // middleware
@@ -47,10 +47,16 @@ async function run() {
         res.send(result);
       }
     });
-
     app.post("/carts", async (req, res) => {
       const item = req.body;
       const result = await cartsCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
     await client.db("admin").command({ ping: 1 });
